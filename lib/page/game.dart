c1578109a4c5f88e27ccf05177bffb1dc4844e23
lenovo <lenovo@mail.com>
2020-12-22 11:32:18 +0700

    add page

---
+++ lib/page/game.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
